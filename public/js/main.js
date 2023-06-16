3fa028e1c3087e103ea802349cbff8757d74e917
ishi12121 <isht1402@gmail.com>
2023-06-16 19:27:29 +0530

    sending pre offer

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,8 @@
 import * as store from './store.js';
 
 import * as wss from './wss.js';
+import * as webRTCHandler from "./webRTCHandler.js";
+import * as constants from './constants.js';
 // initialization of socketIO connection
 const socket = io('/');
 wss.registerSocketEvents(socket);
@@ -11,3 +13,38 @@ personalCodeCopyButton.addEventListener('click', () => {
     const personalCode =  store.getState().socketId;
     navigator.clipboard && navigator.clipboard.writeText(personalCode);
 });
+
+// register event listeners for connection buttons
+
+const personalCodeChatButton = document.getElementById(
+    "personal_code_chat_button"
+  );
+  
+  const personalCodeVideoButton = document.getElementById(
+    "personal_code_video_button"
+  );
+  
+  personalCodeChatButton.addEventListener("click", () => {
+    console.log("chat button clicked");
+  
+    const calleePersonalCode = document.getElementById(
+      "personal_code_input"
+    ).value;
+    const callType = constants.callType.CHAT_PERSONAL_CODE;
+  
+    webRTCHandler.sendPreOffer(callType, calleePersonalCode);
+  });
+  
+  personalCodeVideoButton.addEventListener("click", () => {
+    console.log("video button clicked");
+  
+    const calleePersonalCode = document.getElementById(
+      "personal_code_input"
+    ).value;
+    const callType = constants.callType.VIDEO_PERSONAL_CODE;
+  
+    webRTCHandler.sendPreOffer(callType, calleePersonalCode);
+  });
+  
+
+
---
+++ public/js/webRTCHandler.js
@@ -0,0 +1,10 @@
+import * as wss from "./wss.js";
+import * as constants from "./constants.js";
+import * as ui from "./ui.js";
+
+
+export const sendPreOffer = (callType, calleePersonalCode) => {
+    console.log('pre offer function executed');
+    console.log(callType);
+    console.log(calleePersonalCode);
+};
